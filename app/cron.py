b90378f3b5c949fca7c56c98d27950eda54b8174
Christian Haudenschild <cch360@gmail.com>
2020-04-30 01:36:58 -0400

    cron

--- app/cron.py
+++ app/cron.py
@@ -11,7 +11,7 @@ sched = BlockingScheduler()
 
 
 # Cron job that sends volunteers their delivery details at specified time (Friday morning 6am)
-# @sched.scheduled_job('cron', **app.config['RECIPIENT_EMAIL_SEND_TIME'])
+@sched.scheduled_job('cron', **app.config['RECIPIENT_EMAIL_SEND_TIME'])
 def send_recipient_email():
     print(f'chron_job triggered {dt.datetime.now()}')
     # Transactions this week must have dates earlier than next Thursday 6PM.
@@ -20,21 +20,23 @@ def send_recipient_email():
         d += dt.timedelta(1)
     next_week_cutoff = dt.datetime(d.year, d.month, d.day,
                                    app.config['CUTOFF_DAYTIME']['Hour'])
-    next_week_cutoff = dt.datetime(2020, 5, 3)
+    #next_week_cutoff = dt.datetime(2020, 5, 3)
     transactions = Transaction.query.filter(
         Transaction.date >= dt.datetime.today()).filter(Transaction.date < next_week_cutoff).filter(Transaction.claimed == True)
-    breakpoint()
+
     for transaction in transactions:
 
         date_str = transaction.date.strftime('%A, %m/%d')
         subject = f'Delivery for {transaction.recipient.name} on {date_str}'
-
+        print(subject)
+        '''
         send_email(subject,
                    sender=app.config['ADMINS'][0],
                    recipients=[transaction.volunteer.email],
                    text_body=render_template(
                        'email/volunteer_reminder.txt', date=date_str, transaction=transaction),
                    html_body=render_template('email/volunteer_reminder.html', date=date_str, transaction=transaction))
+        '''
 
 
-# sched.start()
+sched.start()
