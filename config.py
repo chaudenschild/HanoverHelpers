25765499a904465ee93f09bb67ddbedc46c7e178
Christian Haudenschild <cch360@gmail.com>
2020-05-01 18:31:43 -0400

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config():
     ADMINS = ['hanoverhelpers@gmail.com']
     CUTOFF_DAYTIME = {'Day': 3, 'Hour': 18}  # Thursday 6PM
     RECIPIENT_EMAIL_SEND_TIME = {'timezone': 'America/New_York',
-                                 'day_of_week': 'fri', 'hour': '18', 'minute': '31'}  # Should be friday 6am; different for debugging
+                                 'day_of_week': 'fri', 'hour': '18', 'minute': '35'}  # Should be friday 6am; different for debugging
 
 
 # Set environment vars for local testing
